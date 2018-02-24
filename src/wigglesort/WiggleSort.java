b6b7ea088d2bd068724531232fd12014acd2a254
mengranzhu <zzmmrr0504@gmail.com>
2018-02-24 15:09:33 -0800

    280 wiggle sort

---
+++ src/wigglesort/WiggleSort.java
@@ -0,0 +1,28 @@
+package src.wigglesort;
+
+/*
+* Leetcode 280 Wiggle Sort
+* Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >= nums[2] <= nums[3]....
+
+For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
+*
+*when i is even and nums[i] > nums[i+1] swap and i is odd and nums[i] < nums[i+1] swap O(n) timecomplexity
+*
+*
+*
+* */
+public class WiggleSort {
+    public void wiggleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if ((i % 2 == 0 && nums[i] > nums[i + 1]) || (i % 2 == 1 && nums[i] < nums[i + 1])) {
+                swap(nums, i, i + 1);
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
