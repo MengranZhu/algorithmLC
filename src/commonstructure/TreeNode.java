5daf033f79a5cbedcfacccd7a1b7e37c3de6a7f1
mengranzhu <zzmmrr0504@gmail.com>
2018-03-04 14:49:47 -0800

    236 lowest common ancestor of a binary tree

---
+++ src/commonstructure/TreeNode.java
@@ -0,0 +1,12 @@
+package src.commonstructure;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) {
+        val = x;
+    }
+
+}
---
+++ src/microsoft/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,29 @@
+package src.microsoft;
+
+import src.commonstructure.TreeNode;
+import src.tag.BinaryTree;
+
+/*
+ * leetcode 236 Lowest Common Ancestor of a Binary Tree
+ * tree postOrder traversal
+ *first left and right and root
+ * if find left and also find right return the root is the common root
+ *
+ *
+ * */
+@BinaryTree
+public class LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left == null)
+            return right;
+        else if (right == null)
+            return left;
+        else
+            return root;
+
+    }
+}
