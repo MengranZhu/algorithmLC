de075e9a9a412b6f0fb644a46b974a1ffdbad17e
mengranzhu <zzmmrr0504@gmail.com>
2018-03-07 21:59:44 -0800

    Reverse a singly linked list

---
+++ src/facebook/ReverseLinkedList.java
@@ -0,0 +1,27 @@
+package src.facebook;
+/*
+ * leetcode :206. Reverse Linked List
+ *Reverse a singly linked list.
+ *solution: use iterative method
+ *dummy node as previous cur = head when cur != null
+ * next = cur.next cur.next = pre, pre = cur, cur = next
+ * return pre
+ *
+ * */
+
+import src.commonstructure.ListNode;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+
+        }
+        return pre;
+    }
+}
