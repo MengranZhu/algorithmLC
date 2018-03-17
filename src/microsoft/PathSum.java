45a982744c8ddaf48ec7d13b37b113f5ee464753
mengranzhu <zzmmrr0504@gmail.com>
2018-03-16 20:08:00 -0700

    112. Path Sum

---
+++ src/microsoft/PathSum.java
@@ -0,0 +1,27 @@
+package src.microsoft;
+/*
+ * leetcode: 112 Path sum
+ *Given a binary tree and a sum,
+ * determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ *solution: DFS
+ * tree traversal pre-order
+ *
+ *
+ * */
+
+import src.commonstructure.TreeNode;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return helper(root, sum);
+    }
+
+    public boolean helper(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return sum == root.val;
+        boolean left = helper(root.left, sum - root.val);
+        boolean right = helper(root.right, sum - root.val);
+        return (left || right);
+
+    }
+}
