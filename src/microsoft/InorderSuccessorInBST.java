70f74c99f5685821c8dea333396034db6819ed24
mengranzhu <zzmmrr0504@gmail.com>
2018-03-18 11:52:17 -0700

    285. Inorder Successor in BST

---
+++ src/microsoft/InorderSuccessorInBST.java
@@ -0,0 +1,25 @@
+package src.microsoft;
+/*
+ * leetcode: 285. Inorder Successor in BST
+ *Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+ *solution: BST and tree traversal because it is BST, if root.val <= p.val then root is at left of p otherwise left
+ *
+ *
+ * */
+
+import src.commonstructure.TreeNode;
+
+public class InorderSuccessorInBST {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode res = null;
+        while (root != null) {
+            if (root.val <= p.val) {
+                root = root.right;
+            } else {
+                res = root;
+                root = root.left;
+            }
+        }
+        return res;
+    }
+}
